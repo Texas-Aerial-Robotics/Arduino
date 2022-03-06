a8d84ec125a3852b2a6b0c5fe523f6d02571028a
ishani-narwankar <42013894+ishani-narwankar@users.noreply.github.com>
2022-03-06 13:09:12 -0600

    Add files via upload

---
+++ crickit_tar.ino
@@ -0,0 +1,51 @@
+#include <Adafruit_CircuitPlayground.h>
+#include <Adafruit_Circuit_Playground.h>
+#include "Adafruit_Crickit.h"
+#include "seesaw_servo.h"
+
+Adafruit_Crickit crickit;
+seesaw_Servo myservo(&crickit); 
+seesaw_Servo myservo2(&crickit);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("1 Drive demo!");
+  if(!crickit.begin()){
+    Serial.println("ERROR!"); 
+    while(1);
+  }
+  
+  else Serial.println("Crickit started");
+  myservo.attach(CRICKIT_SERVO1); //servo
+  myservo2.attach(CRICKIT_SERVO2);
+  //our default frequency is 1khz
+  crickit.setPWMFreq(CRICKIT_DRIVE1, 1000); //electromagnet
+}
+
+void loop() {
+  //turn all the way on
+  crickit.analogWrite(CRICKIT_DRIVE1, CRICKIT_DUTY_CYCLE_OFF);
+  CircuitPlayground.strip.setPixelColor(0, 0, 255, 0);
+  CircuitPlayground.redLED(true);
+  
+  myservo.write(0);
+  myservo2.write(0);
+  
+  CircuitPlayground.strip.show();
+
+  //turn all the way off
+  crickit.analogWrite(CRICKIT_DRIVE1, CRICKIT_DUTY_CYCLE_MAX);
+  CircuitPlayground.strip.setPixelColor(0, 0, 0, 255);
+  delay(2600);
+
+  
+  myservo.write(180);
+  myservo2.write(180);
+
+  delay(2300);
+  
+  CircuitPlayground.redLED(false);
+  CircuitPlayground.strip.show();
+  
+  //delay(3000);
+}
